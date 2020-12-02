495a36f41605694e705ca7c3f887f8f8155ed7bf
sondh <son.dh@hesperiafreepress.com>
2020-12-02 12:12:42 +0700

    update ui send mail

--- resources/views/mail_contract.blade.php
+++ resources/views/mail_contract.blade.php
@@ -21,34 +21,36 @@
         </a>
     </div>
     <br>
-    <span style="font-size: 16px;color: #000000;line-height: 30px;">
-        <p>
+    <div style="font-size: 16px;color: #000000;line-height: 30px;">
+        <p style="color:#000000;">
             Kính gửi Quý khách <span style="font-weight: bold;">{{$data->name_customer}}</span>,
         </p>
-        <p>
+        <p style="color:#000000;">
             Flexfit chân thành cảm ơn Quý khách đã tin tưởng lựa chọn sản phẩm của chúng tôi.
         </p>
-        <p>
+        <p style="color:#000000;">
             Sự tin tưởng của Quý khách chính là niềm tự hào trong quá trình phát triển và khẳng định thương hiệu Flexfit.
         </p>
-        <p>
+        <p style="color:#000000;">
             Chúng tôi xin phép gửi tới Quý khách thông tin bảo hành của Hợp đồng số <span style="font-weight: bold;">{{$data->contract_code}} </span> được hoàn thành vào ngày <span style="font-weight: bold;">{{date('d/m/Y', strtotime($data->finish_date))}}</span>.
         </p>
-        <p>
+        <p style="color:#000000;">
            Quý khách vui lòng nhấp vào ảnh phía trên để biết thêm thông tin chi tiết.
         </p>
-        <p>
+        <p style="color:#000000;">
           Chúng tôi luôn sẵn sàng hỗ trợ Quý khách, nếu Quý khách có bất cứ thắc mắc hay vấn đề gì trong quá trình sử dụng, trong trường hợp cần hỗ trợ, vui lòng liên hệ với bộ phận Chăm sóc khách hàng của chúng tôi qua số điện thoại: 1900 633 588.
         </p>
-        <p>
+        <p style="color:#000000;">
           Đội ngũ Flexfit chân thành cảm ơn và hân hạnh được đồng hành cùng Quý khách kiến tạo nên những tổ ấm thẩm mỹ và tiện nghi!
         </p>
-      </p>
-      Trân trọng,
-      <p>
-          Flexfit
-      </p>
-    </span>
-    <span style="font-weight:bold;font-size: 14px;"> Xin vui lòng click vào link <a href='http://baohanh.flexfit.vn/hop-dong/{{$data->contract_code}}' target="_blank">http://baohanh.flexfit.vn/hop-dong/{{$data->contract_code}}</a> để đến trang bảo hành chi tiết . Xin chân thành cảm ơn ! </span>
+        </pstyle="color:#000000;">
+            Trân trọng,
+        <p style="color:#000000;">
+            Flexfit
+        </p>
+        <br>
+        <span style="font-weight:bold;font-size: 14px;color:#000000;"> Xin vui lòng click vào link <a href='http://baohanh.flexfit.vn/hop-dong/{{$data->contract_code}}' target="_blank">http://baohanh.flexfit.vn/hop-dong/{{$data->contract_code}}</a> để đến trang bảo hành chi tiết . Xin chân thành cảm ơn ! </span>
+    </div>
+    
 </body>
 </html>
